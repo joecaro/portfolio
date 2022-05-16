ad83b25f055d1f2eac4e29eea36caeecd9f0209f
unknown <jsc11598@gmail.com>
2022-05-16 16:40:58 -0400

    project card desktop layout

--- components/ProjectCard.js
+++ components/ProjectCard.js
@@ -7,47 +7,51 @@ export default function ProjectCard({ project }) {
     <Container>
       <ProjectImage></ProjectImage>
       <Link
-        target="_blank"
-        rel="noreferrer"
+        target='_blank'
+        rel='noreferrer'
         href={`/projects/${project.slug}`}
-        passHref
-      >
-        <div className="title">{project.title}</div>
+        passHref>
+        <div className='title'>{project.title}</div>
       </Link>
-      <div className="links">
-        <ButtonLink color="purple" target="_blank" rel="noreferrer" href={project.github || a}>
+      <div className='links'>
+        <ButtonLink
+          color='purple'
+          target='_blank'
+          rel='noreferrer'
+          href={project.github || a}>
           <svg
-            xmlns="http://www.w3.org/2000/svg"
-            width="30"
-            height="30"
-            fill="currentColor"
-            viewBox="0 0 16 16"
-          >
-            <path d="M8 0C3.58 0 0 3.58 0 8c0 3.54 2.29 6.53 5.47 7.59.4.07.55-.17.55-.38 0-.19-.01-.82-.01-1.49-2.01.37-2.53-.49-2.69-.94-.09-.23-.48-.94-.82-1.13-.28-.15-.68-.52-.01-.53.63-.01 1.08.58 1.23.82.72 1.21 1.87.87 2.33.66.07-.52.28-.87.51-1.07-1.78-.2-3.64-.89-3.64-3.95 0-.87.31-1.59.82-2.15-.08-.2-.36-1.02.08-2.12 0 0 .67-.21 2.2.82.64-.18 1.32-.27 2-.27.68 0 1.36.09 2 .27 1.53-1.04 2.2-.82 2.2-.82.44 1.1.16 1.92.08 2.12.51.56.82 1.27.82 2.15 0 3.07-1.87 3.75-3.65 3.95.29.25.54.73.54 1.48 0 1.07-.01 1.93-.01 2.2 0 .21.15.46.55.38A8.012 8.012 0 0 0 16 8c0-4.42-3.58-8-8-8z" />
+            xmlns='http://www.w3.org/2000/svg'
+            width='30'
+            height='30'
+            fill='currentColor'
+            viewBox='0 0 16 16'>
+            <path d='M8 0C3.58 0 0 3.58 0 8c0 3.54 2.29 6.53 5.47 7.59.4.07.55-.17.55-.38 0-.19-.01-.82-.01-1.49-2.01.37-2.53-.49-2.69-.94-.09-.23-.48-.94-.82-1.13-.28-.15-.68-.52-.01-.53.63-.01 1.08.58 1.23.82.72 1.21 1.87.87 2.33.66.07-.52.28-.87.51-1.07-1.78-.2-3.64-.89-3.64-3.95 0-.87.31-1.59.82-2.15-.08-.2-.36-1.02.08-2.12 0 0 .67-.21 2.2.82.64-.18 1.32-.27 2-.27.68 0 1.36.09 2 .27 1.53-1.04 2.2-.82 2.2-.82.44 1.1.16 1.92.08 2.12.51.56.82 1.27.82 2.15 0 3.07-1.87 3.75-3.65 3.95.29.25.54.73.54 1.48 0 1.07-.01 1.93-.01 2.2 0 .21.15.46.55.38A8.012 8.012 0 0 0 16 8c0-4.42-3.58-8-8-8z' />
           </svg>
         </ButtonLink>
 
-        <ButtonLink inverted target="_blank" rel="noreferrer" href={project.demo || ""}>
+        <ButtonLink
+          inverted
+          target='_blank'
+          rel='noreferrer'
+          href={project.demo || ""}>
           <svg
-            xmlns="http://www.w3.org/2000/svg"
-            width="30"
-            height="30"
-            fill="currentColor"
-            viewBox="0 0 16 16"
-          >
-            <path d="M6.354 5.5H4a3 3 0 0 0 0 6h3a3 3 0 0 0 2.83-4H9c-.086 0-.17.01-.25.031A2 2 0 0 1 7 10.5H4a2 2 0 1 1 0-4h1.535c.218-.376.495-.714.82-1z" />
-            <path d="M9 5.5a3 3 0 0 0-2.83 4h1.098A2 2 0 0 1 9 6.5h3a2 2 0 1 1 0 4h-1.535a4.02 4.02 0 0 1-.82 1H12a3 3 0 1 0 0-6H9z" />
+            xmlns='http://www.w3.org/2000/svg'
+            width='30'
+            height='30'
+            fill='currentColor'
+            viewBox='0 0 16 16'>
+            <path d='M6.354 5.5H4a3 3 0 0 0 0 6h3a3 3 0 0 0 2.83-4H9c-.086 0-.17.01-.25.031A2 2 0 0 1 7 10.5H4a2 2 0 1 1 0-4h1.535c.218-.376.495-.714.82-1z' />
+            <path d='M9 5.5a3 3 0 0 0-2.83 4h1.098A2 2 0 0 1 9 6.5h3a2 2 0 1 1 0 4h-1.535a4.02 4.02 0 0 1-.82 1H12a3 3 0 1 0 0-6H9z' />
           </svg>
-
         </ButtonLink>
       </div>
-      <p className="description">{project.description}</p>
+      <p className='description'>{project.description}</p>
 
-      <div className="stack-container">
+      <div className='stack-container'>
         <b>STACK |</b>
-        <div className="stack">
+        <div className='stack'>
           {project.stack.map((tech) => (
-            <p key={`${project.title} ${tech}`} className="stack-item">
+            <p key={`${project.title} ${tech}`} className='stack-item'>
               {tech}
             </p>
           ))}
@@ -64,19 +68,24 @@ const Container = styled.div`
     "image links"
     "desciption desciption"
     "stack stack";
-  grid-template-rows: 50px 1fr 1fr 1fr;
+  gap: 1rem;
+  grid-template-rows: 100px 1fr 1fr 1fr;
   grid-template-columns: 2fr 3fr;
   place-items: center;
 
   border-radius: var(--radiusMd);
   border-bottom: 4px solid var(--gray700);
-  padding: .25rem;
+  padding: 0.25rem;
 
   .title {
     align-self: start;
     grid-area: title;
     font-size: 1.5rem;
-    padding: .5rem;
+    font-weight: 600;
+    padding: 0.5rem;
+    @media (min-width: 900px) {
+      justify-self: start;
+    }
   }
 
   .links {
@@ -85,6 +94,9 @@ const Container = styled.div`
 
   .description {
     grid-area: desciption;
+    @media (min-width: 900px) {
+      justify-self: start;
+    }
   }
 
   .stack-container {
@@ -116,6 +128,15 @@ const Container = styled.div`
     :hover {
       text-decoration: underline;
     }
+  }
+  @media (min-width: 900px) {
+    grid-template-rows: 50px 1fr 1fr 1fr;
+    grid-template-columns: 2fr 3fr 2fr;
+    grid-template-areas:
+      "image title blank"
+      "image desciption links"
+      "image desciption links"
+      "stack stack stack";
   }
 `;
 
