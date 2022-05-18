f8669ee94dbea100afd3b87da611b757a89dbd3e
unknown <jsc11598@gmail.com>
2022-05-18 17:10:47 -0400

    update tests to remove label attribute

--- components/_tests_/ButtonLink.spec.js
+++ components/_tests_/ButtonLink.spec.js
@@ -4,42 +4,59 @@ import ButtonLink from "../ButtonLink";
 
 describe("ButtonLink", () => {
   let expectedProps;
-  
-    beforeEach(() => {
+
+  beforeEach(() => {
     expectedProps = {
       color: "blue",
       href: "www.example.com",
-      label: "TEXT",
     };
   });
 
   test("renders correctly with all props & children", () => {
-    const { container, getByText, getByTestId } = render(<ButtonLink {...expectedProps}><p data-testid="child"></p></ButtonLink>);
-    expect(getByText("TEXT")).toBeInTheDocument();
+    const { container, getByTestId } = render(
+      <ButtonLink {...expectedProps}>
+        <p data-testid='child'></p>
+      </ButtonLink>
+    );
     expect(getByTestId("child")).toBeInTheDocument();
-    expect(container.firstChild.firstChild).toHaveAttribute("color", "blue")
-    expect(container.firstChild.firstChild).toHaveAttribute("href", "www.example.com")
+    expect(container.firstChild.firstChild).toHaveAttribute("color", "blue");
+    expect(container.firstChild.firstChild).toHaveAttribute(
+      "href",
+      "www.example.com"
+    );
   });
 
   test("renders grey when no color provided", () => {
-    const { container, getByText, getByTestId } = render(<ButtonLink href="www.example.com" label="TEXT"><p data-testid="child"></p></ButtonLink>);
-    expect(getByText("TEXT")).toBeInTheDocument();
+    const { container, getByTestId } = render(
+      <ButtonLink href='www.example.com' label='TEXT'>
+        <p data-testid='child'></p>
+      </ButtonLink>
+    );
     expect(getByTestId("child")).toBeInTheDocument();
-    expect(container.firstChild.firstChild).toHaveAttribute("color", "gray")
-    expect(container.firstChild.firstChild).toHaveAttribute("href", "www.example.com")
+    expect(container.firstChild.firstChild).toHaveAttribute("color", "gray");
+    expect(container.firstChild.firstChild).toHaveAttribute(
+      "href",
+      "www.example.com"
+    );
   });
   test("renders gray with no animation (disabled) when no link provided", () => {
-    const { container, getByText, getByTestId } = render(<ButtonLink color={expectedProps.color} label={expectedProps.label}><p data-testid="child"></p></ButtonLink>);
-    expect(getByText("TEXT")).toBeInTheDocument();
+    const { container, getByTestId } = render(
+      <ButtonLink color={expectedProps.color} label={expectedProps.label}>
+        <p data-testid='child'></p>
+      </ButtonLink>
+    );
     expect(getByTestId("child")).toBeInTheDocument();
-    expect(container.firstChild.firstChild).toHaveAttribute("color", "blue")
-    expect(container.firstChild.firstChild).toHaveClass("disabled")
+    expect(container.firstChild.firstChild).toHaveAttribute("color", "blue");
+    expect(container.firstChild.firstChild).toHaveClass("disabled");
   });
   test("renders inverted (colored border) when passed inverted prop", () => {
-    const { container, getByText, getByTestId } = render(<ButtonLink {...expectedProps} inverted><p data-testid="child"></p></ButtonLink>);
-    expect(getByText("TEXT")).toBeInTheDocument();
+    const { container, getByTestId } = render(
+      <ButtonLink {...expectedProps} inverted>
+        <p data-testid='child'></p>
+      </ButtonLink>
+    );
     expect(getByTestId("child")).toBeInTheDocument();
-    expect(container.firstChild.firstChild).toHaveAttribute("color", "blue")
+    expect(container.firstChild.firstChild).toHaveAttribute("color", "blue");
     expect(container).toHaveStyle("border: 3px solid var(--blue400);");
-  })
+  });
 });
