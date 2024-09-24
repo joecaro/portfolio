dd35ebcae38957aff6efbca594f62dbd16f8b251
Joseph Carothers <jsc11598@gmail.com>
2024-09-24 02:55:18 -0400

    update about me

--- src/components/AboutMe.tsx
+++ src/components/AboutMe.tsx
@@ -5,24 +5,26 @@ export default function AboutMe() {
         <div className='dark:text-neutral-300 pb-40 sm:pb-40'>
             <h1 className='text-3xl mb-4'>Joseph Carothers</h1>
             <p>
-                I am a <strong> web developer</strong> creating interactive
-                applications, company websites, and company tools. {"I've "}
-                focused primarily on front-ent applications, but am comfortable
-                working with back-end technologies having created serveral
-                backend applications using the ExpressjS framework connecting to
-                both SQL and NOSQL databases.
+                I’m a web developer specializing in creating interactive
+                applications, company websites, and internal tools. While I
+                primarily focus on front-end development, I’m equally
+                comfortable working on the back-end, having built multiple
+                server-side applications using the ExpressJS framework,
+                integrating with both SQL and NoSQL databases.
             </p>
             <p className='mt-2'>
-                Recently I{"'"}ve worked at Action Network on projects ranging
-                from tools empowering SEO teams to deliver high value content to
-                powering a distributed promotion rendering platform using Web
-                Components which run on a range of clients from wordpress to
-                react applications.
+                Recently, I’ve worked at Action Network on a range of projects,
+                from building tools that empower SEO teams to deliver high-value
+                content, to developing a distributed promotion rendering
+                platform powered by Web Components. This platform seamlessly
+                runs across various clients, including WordPress and React
+                applications.
             </p>
             <p className='mt-2'>
-                I find fulfillment in taking on topics that expand my skills and
-                knowledge. Beyond web development, you can see the product of
-                some of my other interests below.
+                I’m passionate about continually expanding my skills and
+                knowledge, and I thrive on tackling challenging projects. Beyond
+                web development, I explore a variety of other interests, which
+                you can see reflected in my work below.
             </p>
         </div>
     );
